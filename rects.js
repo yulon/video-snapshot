cfd1fae4fcde72de28774fdab0ef7431be4add0e
Yuchen Wang <yulon@me.com>
2017-05-05 02:06:38 +0800

    Updated video elements elector

--- rects.js
+++ rects.js
@@ -1,22 +1,16 @@
-var videos = document.querySelectorAll('video');
-var flashs = document.querySelectorAll('object[type="application/x-shockwave-flash"]');
-
+var ves = document.querySelectorAll('video, [type="application/x-shockwave-flash"]');
 var rects = new Array();
 
-function getRects(eles) {
-	for (var i = 0; i < eles.length; i++) {
-		var rect = eles[i].getBoundingClientRect();
-		if (
-			window.innerWidth > rect.left &&
-			rect.left + rect.width > 0 &&
-			window.innerHeight > rect.top &&
-			rect.top + rect.height > 0
-		) {
-			rects.push({ left: rect.left, top: rect.top, width: rect.width, height: rect.height });
-		}
+for (var i = 0; i < ves.length; i++) {
+	var rect = ves[i].getBoundingClientRect();
+	if (
+		window.innerWidth > rect.left &&
+		rect.left + rect.width > 0 &&
+		window.innerHeight > rect.top &&
+		rect.top + rect.height > 0
+	) {
+		rects.push({ left: rect.left, top: rect.top, width: rect.width, height: rect.height });
 	}
 }
-getRects(videos);
-getRects(flashs);
 
 chrome.runtime.sendMessage(null, rects);
