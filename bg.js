d85e8fb72532b2790bf5ce9601b696d8fe3c1e3a
Yulon <yulon@me.com>
2019-04-11 19:25:12 +0800

    Fixed download

--- bg.js
+++ bg.js
@@ -12,29 +12,35 @@ function capture(popup) {
 	crxCS.insert(null, { file: "capture.js" }, function() {
 		crxCS.callA(null, "get", function(result) {
 
-			var scrShot, zm, bufCav, bufCavCtx;
+			var scrShot, zm;
 
 			function mkImgList() {
 				for (var i = 0; i < result.vidShots.length; i++) {
-					var img = new popup.Image();
-					img.onload = function() {
-						this.style.height = this.naturalHeight / (this.naturalWidth / 400) + "px";
-					};
+					var bufCav = popup.document.createElement("canvas");
 
 					if (result.vidShots[i].constructor === String) {
+						var img = new popup.Image();
+						img.onload = function() {
+							bufCav.width = this.naturalWidth;
+							bufCav.height = this.naturalHeight;
+							bufCav.style.height = this.naturalHeight / (this.naturalWidth / 400) + "px";
+							bufCav.getContext("2d").drawImage(this, 0, 0);
+						};
 						img.src = result.vidShots[i];
 					} else {
 						bufCav.width = result.vidShots[i].width * zm;
 						bufCav.height = result.vidShots[i].height * zm;
-						bufCavCtx.drawImage(scrShot, -result.vidShots[i].left * zm, -result.vidShots[i].top * zm);
-						img.src = bufCav.toDataURL('image/png');
+						bufCav.style.height = result.vidShots[i].height / (result.vidShots[i].width / 400) + "px";
+						bufCav.getContext("2d").drawImage(scrShot, -result.vidShots[i].left * zm, -result.vidShots[i].top * zm);
 					}
 
-					popup.document.body.appendChild(img);
+					popup.document.body.appendChild(bufCav);
 				}
 				popup.onclick = function(mouseEvent) {
-					if (mouseEvent.target.tagName === "IMG") {
-						chrome.downloads.download({ url: mouseEvent.target.src, saveAs: true, filename: "chrome_video_capture_" + (new Date()).getTime() + ".png" });
+					if (mouseEvent.target.tagName === "CANVAS") {
+						mouseEvent.target.toBlob(function(blob) {
+							downloadBlob("chrome_video_capture_" + (new Date()).getTime() + ".png", blob);
+						}, "image/png");
 					}
 				};
 				popup.onunload = function(mouseEvent) {
@@ -43,17 +49,12 @@ function capture(popup) {
 			}
 
 			if (result.needScrShot) {
-				bufCav = popup.document.createElement("canvas");
-				bufCavCtx = bufCav.getContext("2d");
-
 				chrome.tabs.captureVisibleTab({ format: "png" }, function(dataUrl) {
 					scrShot = new Image();
 					scrShot.onload = function() {
 						chrome.tabs.getZoom(function(zoomFactor) {
 							zm = zoomFactor;
-							callOnLoad(function() {
-								mkImgList(zoomFactor);
-							});
+							callOnLoad(mkImgList);
 						});
 					};
 					scrShot.src = dataUrl;
@@ -65,4 +66,13 @@ function capture(popup) {
 			}
 		});
 	});
+}
+
+function downloadBlob(fileName, blob) {
+	var aLink = document.createElement('a');
+	var evt = document.createEvent('MouseEvents');
+	evt.initEvent('click', true, true);
+	aLink.download = fileName;
+	aLink.href = URL.createObjectURL(blob);
+	aLink.dispatchEvent(evt);
 }
